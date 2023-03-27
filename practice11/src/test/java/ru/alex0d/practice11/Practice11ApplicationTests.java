fb6d3785c99ff2e6273f82fb09319f44031de7d4
alex0d <alexey.d707@gmail.com>
2023-03-27 19:59:09 +0300

    Added Practice 11

---
+++ practice11/src/test/java/ru/alex0d/practice11/Practice11ApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.alex0d.practice11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Practice11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
